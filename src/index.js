223df17ac9966c421beb66a5d3db741102481813
unknown <you@example.com>
2018-10-14 13:41:28 -0500

    Se agrega metodo para filtro de notas

--- src/index.js
+++ src/index.js
@@ -30,14 +30,8 @@ class EuNews {
 
 
   filterNews(category){
-    return [{
-      slug: '/noticias/2018/09/19/nota/6959801/arte-tejedoras-paja-toquilla-muestra',
-      category: 'noticias'
-    },{
-      slug: '/noticias/2018/09/19/nota/6959801/arte-tejedoras-paja-toquilla-muestra-1',
-      category: 'noticias'
-    }]
-  }
+    return this.news.filter(item => item.category === category); 
+    }
   
   count() {
     return this.news.length;
